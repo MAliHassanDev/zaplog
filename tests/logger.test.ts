43e900dc2f3a3489ed76140eb1d194f1c4e175f6
ali <muhammadalihassanofficial@gmail.com>
2024-12-15 17:52:11 +0500

    test: Logger and LoggerOptions

--- tests/logger.test.ts
+++ tests/logger.test.ts
@@ -1,13 +1,67 @@
-import { describe, it, expect } from "vitest";
+import { describe, vi, it, expect, beforeEach, afterEach } from "vitest";
+import Logger, { LoggerOptions } from "../src/logger";
 
-describe("test", () => {
-  it("should pass", () => {
-    expect.assertions(1);
-    expect(10).toBe(10);
+describe("Logger", () => {
+  describe("given level info", () => {
+    const spy = vi.spyOn(console, "log");
+    let logger: Logger;
+    let message: string;
+
+    beforeEach(() => {
+      message = "mock message";
+      logger = new Logger({ level: "info" });
+    });
+
+    afterEach(() => {
+      vi.resetAllMocks();
+    });
+    it("on info method call logs the message with title info to console", () => {
+      logger.info(message);
+      expect(spy).toHaveBeenCalled();
+      expect(vi.mocked(spy).mock.calls[0][0]).toMatch(message);
+      expect(vi.mocked(spy).mock.calls[0][0]).toMatch(/info/);
+    });
+
+    it("on warn method call, logs the message with title warn to console", () => {
+      logger.warn(message);
+      expect(spy).toHaveBeenCalled();
+      expect(vi.mocked(spy).mock.calls[0][0]).toMatch(message);
+      expect(vi.mocked(spy).mock.calls[0][0]).toMatch(/warn/);
+    });
+  });
+});
+
+describe("LoggerOptions", () => {
+  describe("in development environment", () => {
+    it("returns default dev options", () => {
+      const defaultOptions = {
+        level: "info",
+        errorStack: true,
+      };
+      const loggerOptions = new LoggerOptions("development");
+      expect(loggerOptions.getDefault()).toMatchObject(defaultOptions);
+    });
   });
 
-  it("should also pass", () => {
-    expect.assertions(1);
-    expect(10).toBe(10);
+  describe("in production environment", () => {
+    it("returns default prod options", () => {
+      const defaultOptions = {
+        level: "warn",
+        errorStack: true,
+      };
+      const loggerOptions = new LoggerOptions("production");
+      expect(loggerOptions.getDefault()).toMatchObject(defaultOptions);
+    });
+  });
+
+  describe("in testing environment", () => {
+    it("returns default test options", () => {
+      const defaultOptions = {
+        level: "error",
+        errorStack: true,
+      };
+      const loggerOptions = new LoggerOptions("test");
+      expect(loggerOptions.getDefault()).toMatchObject(defaultOptions);
+    });
   });
 });
--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,40 +1,6 @@
 import { it, describe, expect } from "vitest";
-import {
-  getDefaultLoggerOptions,
-  getLogLevel,
-  getTimeStamp,
-  NodeEnv,
-  transformIn2Digit,
-} from "../src/util";
-import { LoggerOptions } from "../src/logger";
+import { getTimeStamp, transformIn2Digit } from "../src/util";
 
-describe("getLogLevel", () => {
-  it("returns 'info' on invalid env", () => {
-    expect(getLogLevel("invalid" as NodeEnv)).toBe("info");
-  });
-
-  it("returns 'info' when env is development", () => {
-    expect(getLogLevel("development")).toBe("info");
-  });
-
-  it("returns 'warn' when env is production", () => {
-    expect(getLogLevel("production")).toBe("warn");
-  });
-
-  it("returns 'error' when env is test", () => {
-    expect(getLogLevel("test")).toBe("error");
-  });
-});
-
-describe("getDefaultLoggerOptions", () => {
-  it("returns default logger options on valid env", () => {
-    const defaultOptions: LoggerOptions = {
-      level: "info",
-      errorStack: true,
-    };
-    expect(getDefaultLoggerOptions("development")).toEqual(defaultOptions);
-  });
-});
 describe("transformIn2Digit", () => {
   it("returns number in 2 digit format if number is less than 10", () => {
     expect(transformIn2Digit(6)).toBe("06");
@@ -47,8 +13,8 @@ describe("transformIn2Digit", () => {
 
 describe("getTimeStamp", () => {
   it("returns timeStamp in the given format", () => {
-    const date = new Date();
-    const stamp = `${transformIn2Digit(date.getDate())}-${transformIn2Digit(date.getMonth())}-${date.getFullYear()}`;
+    const date = new Date("02-23-2021");
+    const stamp = "23-01-2021";
     expect(getTimeStamp("dd-MM-yyyy", date)).toBe(stamp);
   });
 });
