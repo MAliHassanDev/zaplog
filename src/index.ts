c5896306e124182c8e592f90f839b0c5dc9534cd
ali <muhammadalihassanoffiial@gmail.com>
2025-03-11 19:03:51 +0500

    refactor: make node the default value of env arg in createLogger

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import {
 } from "./logger/types/logger.types.js";
 import { isEnvironmentNode } from "./utils/util.js";
 
+export * from "./logger/types/logger.types.js";
+
 const Logger = isEnvironmentNode()
   ? (await import("./logger/node/nodeLogger.js")).NodeLogger
   : (await import("./logger/browser/browserLogger.js")).BrowserLogger;
@@ -15,12 +17,15 @@ export function createLogger(
   config?: BrowserLoggerConfig,
 ): Logger;
 export function createLogger(
-  loggerEnvironment: "node",
+  loggerEnvironment?: "node",
   config?: NodeLoggerConfig,
 ): Logger;
 export function createLogger(
-  loggerEnvironment: LoggerEnvironment,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  loggerEnvironment: LoggerEnvironment = "node",
   config?: BrowserLoggerConfig | NodeLoggerConfig,
 ) {
   return new Logger(config) as Logger;
 }
+
+
